bed00f32da0bbbb0ffabdd3bc1602c7e2c275020
Slava Pavlutin <sl.pavlutin@gmail.com>
2025-06-13 10:38:35 +0300

    Add clamp

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,3 +2,8 @@
 export function lerp(a: number, b: number, t: number) {
   return a + (b - a) * t;
 }
+
+/** Clamp value between min and max */
+export function clamp(value: number, min: number, max: number) {
+  return Math.min(Math.max(value, min), max);
+}
